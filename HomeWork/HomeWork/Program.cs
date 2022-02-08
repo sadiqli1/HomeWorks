a5e86cbc48984442c9fee87c0414ecb33fa07dcf
sadiqli1 <isaas@code.edu.az>
2022-02-09 02:40:14 +0400

    created HomeWork

---
+++ HomeWork/HomeWork/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            string txt = "kitab";
+
+            for (int i = 0; i <= txt.Length; i++)
+            {
+                if (txt[i] == 'a')
+                {
+                    Console.WriteLine("a var");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("a yoxdur");
+                }
+            }
+
+            //task2
+            int num = 132;
+            if (num % 2 ==0)
+            {
+                Console.WriteLine("cutdur");
+            }
+            else
+            {
+                Console.WriteLine("tekdir");
+            }
+            
+        }
+        
+    }
+}
