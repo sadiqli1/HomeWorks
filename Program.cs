a58c76600ceed8a2a045dc8d0b618639922d1307
sadiqli1 <98384508+sadiqli1@users.noreply.github.com>
2022-02-10 03:09:58 +0400

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            int count = 1;
+            int[] newArr = new int[count];
+            if (num <= 50)
+            {
+                for (int i = 4; i < num; i++)
+                {
+                    if (i % 3 == 0)
+                    {
+                        count++;
+                        newArr[i] = i;
+
+                    }
+                }
+            }
+            else if (num > 50 && num <= 100)
+            {
+                for (int i = 6; i < num; i++)
+                {
+                    if (i % 5 == 0)
+                    {
+                        count++;
+                        newArr[i] = i;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 9; i < num; i++)
+                {
+                    if (i % 8 == 0)
+                    {
+                        count++;
+                        newArr[i] = i;
+                    }
+                }
+            }
+            foreach (int divided in newArr)
+            {
+                Console.WriteLine(divided);
+            }
+            #endregion
+
+            #region task2
+
+            //string[] words = {"kitab" , "stol" , "defter" , "telefon" , "kart" };
+
+            //foreach (string word in words)
+            //{
+            //    for (int i = 0; i < word.Length; i++)
+            //    {
+            //        Console.WriteLine(i);
+
+            //    }             
+            //}
+
+            #endregion
+
+            #region task3
+
+            //int num = 214;
+            //int sum = 0;
+
+
+            #endregion
+
+        }
+    }
+}
